fdf107da0174f4853f98a903a75a463c0cd73525
kaladin <335095@niuitmo.ru>
2024-09-15 16:37:36 +0300

    setup commands

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,11 +1,11 @@
 const { Telegraf, Markup } = require("telegraf");
-const TOKEN = "7390776943:AAGvJKezEPGBmKlVdRqGHx1DMlIM-geVguQ";
+const TOKEN = "7308271882:AAEjGS7sOSHp5Amfq0XmjNZum56fb4mwvi4";
 const bot = new Telegraf(TOKEN);
 const express = require("express");
 const app = express()
 app.use(express.json())
-const web_link = "https://tgame-chi.vercel.app";
-const community_link = "https://t.me/geto_spirit_announcement"; 
+const web_link = "https://temaatica.github.io/frontend/";
+const community_link = "https://github.com/TeMaAtica/frontend"; 
 
 
 bot.start((ctx) => {
@@ -13,13 +13,13 @@ bot.start((ctx) => {
     const urlSent = `${web_link}?ref=${startPayload}`;
     const user = ctx.message.from;
     const userName = user.username ? `@${user.username}` : user.first_name;
-    ctx.replyWithMarkdown(`*Hey, Welcome to Geto, tap to get your coin
+    ctx.replyWithMarkdown(`Hey, Welcome to Geto, tap to get your coin
 Got friends, relatives, co-workers?
 Bring them all into the game.
 More buddies, more coins.`, {
         reply_markup: {
             inline_keyboard: [
-              [{ text: "👋 Start now!", web_app: { url: urlSent } }],
+              [{ text: "👋 TEMATIKA zxc!", web_app: { url: urlSent } }],
               [{ text: "Join our Community", url: community_link }]
             
             ],
