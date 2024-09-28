064e401dc7a470927255c7450a2fdc937cd23f14
Данил Соловьев <89980506+ziperlin17@users.noreply.github.com>
2024-09-28 15:22:00 +0300

    Update index.js

--- src/images/index.js
+++ src/images/index.js
@@ -1,22 +1,17 @@
-import plutos from '../images/plutotap.svg'
-import bronze from '../images/bronze.webp'
-import Silver from '../images/sliver.webp'
-import gold from '../images/gold.webp'
-import diamond from '../images/diamond.webp'
-import platinum from '../images/platinum.webp'
-import master from '../images/master.webp'
-import tapcoin from '../images/tapme1.webp'
-import lihgt from '../images/lihgt.webp'
-// import plutos from '../images/'
-// import plutos from '../images/'
-// import plutos from '../images/'
-// import plutos from '../images/'
-// import plutos from '../images/'
+const plutos = "/plutotap.svg";
+const bronze = "/bronze.webp";
+const sliver = "/sliver.webp"; 
+const gold = "/gold.webp";
+const diamond = "/diamond.webp";
+const platinum = "/platinum.webp";
+const master = "/master.webp";
+const tapcoin = "/tapme1.webp";
+const lihgt = "/lihgt.webp";
 
 export {
     plutos,
     bronze,
-    Silver,
+    sliver,
     gold,
     diamond,
     platinum,
